37f63e086451902fd79b7d65f9bd99c6f126cd0b
harshloomba <hloomba@gmu.edu>
2017-07-08 16:16:50 -0700

    added routes and layouts

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/login',   to: 'static_pages#login'
+  get  '/signup',   to: 'users#new'
 end
