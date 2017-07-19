44efe75ac736ef0ade46e67f57024227328103e1
harshloomba <hloomba@gmu.edu>
2017-07-18 17:04:58 -0700

    added signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/login',   to: 'static_pages#login'
   get  '/signup',   to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
